ffd552ab959694ea16090de45bbae611f41bda16
Yusuf Ciftci <yciftci@k2m18s06.42kocaeli.com.tr>
2023-03-04 15:47:35 +0300

    unfinished yet

--- main/tag_finder.c
+++ main/tag_finder.c
@@ -6,7 +6,7 @@
 /*   By: yciftci <yciftci@student.42kocaeli.com.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/02 16:12:46 by yciftci           #+#    #+#             */
-/*   Updated: 2023/03/04 15:20:34 by yciftci          ###   ########.fr       */
+/*   Updated: 2023/03/04 15:40:21 by yciftci          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,24 +55,4 @@ void	tag_finder(t_stack **root, int argc)
 		tag++;
 		argc--;
 	}
-}
-
-int main()
-{
-	t_stack *new = NULL;
-	ft_stack_push(&new, ft_stack_new(41));
-	ft_stack_push(&new, ft_stack_new(361));
-	ft_stack_push(&new, ft_stack_new(9));
-	ft_stack_push(&new, ft_stack_new(99));
-	ft_stack_push(&new, ft_stack_new(88));
-	ft_stack_push(&new, ft_stack_new(2));
-
-	//!
-	tag_finder(&new, 7);
-	ft_printf("tag: %d num %d\n", ft_stack_peek(&new)->tag, ft_stack_peek(&new)->num);
-	ft_printf("tag: %d num %d\n", ft_stack_peek(&new)->next->tag, ft_stack_peek(&new)->next->num);
-	ft_printf("tag: %d num %d\n", ft_stack_peek(&new)->next->next->tag, ft_stack_peek(&new)->next->next->num);
-	ft_printf("tag: %d num %d\n", ft_stack_peek(&new)->next->next->next->tag, ft_stack_peek(&new)->next->next->next->num);
-	ft_printf("tag: %d num %d\n", ft_stack_peek(&new)->next->next->next->next->tag, ft_stack_peek(&new)->next->next->next->next->num);
-	ft_printf("tag: %d num %d\n", ft_stack_peek(&new)->next->next->next->next->next->tag, ft_stack_peek(&new)->next->next->next->next->next->num);
 }
